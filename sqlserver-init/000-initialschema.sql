c72685625aec5d6cee8a0212ca5c21baf61b004c
Dustin Masters <dustin.masters@olo.com>
2025-01-30 10:16:24 -0500

    add existing db check to init script

--- sqlserver-init/000-initialschema.sql
+++ sqlserver-init/000-initialschema.sql
@@ -1,37 +1,33 @@
-CREATE DATABASE MyDatabase;
-GO
+IF DB_ID('MyDatabase') IS NULL
+BEGIN
+    CREATE DATABASE MyDatabase;
 
-USE MyDatabase;
-GO
+    USE MyDatabase;
 
-CREATE TABLE MyTable (
-    Id INT PRIMARY KEY,
-    Name NVARCHAR(100),
-    CreatedAt DATETIME
-);
-GO
+    CREATE TABLE MyTable (
+        Id INT PRIMARY KEY,
+        Name NVARCHAR(100),
+        CreatedAt DATETIME
+    );
 
-INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (1, 'Test Name 1', GETDATE());
-INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (2, 'Test Name 2', GETDATE());
-INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (3, 'Test Name 3', GETDATE());
-INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (4, 'Test Name 4', GETDATE());
-INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (5, 'Test Name 5', GETDATE());
-INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (6, 'Test Name 6', GETDATE());
-INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (7, 'Test Name 7', GETDATE());
-INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (8, 'Test Name 8', GETDATE());
-INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (9, 'Test Name 9', GETDATE());
-INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (10, 'Test Name 10', GETDATE());
-INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (11, 'Test Name 11', GETDATE());
-INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (12, 'Test Name 12', GETDATE());
-INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (13, 'Test Name 13', GETDATE());
-GO
+    INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (1, 'Test Name 1', GETDATE());
+    INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (2, 'Test Name 2', GETDATE());
+    INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (3, 'Test Name 3', GETDATE());
+    INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (4, 'Test Name 4', GETDATE());
+    INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (5, 'Test Name 5', GETDATE());
+    INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (6, 'Test Name 6', GETDATE());
+    INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (7, 'Test Name 7', GETDATE());
+    INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (8, 'Test Name 8', GETDATE());
+    INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (9, 'Test Name 9', GETDATE());
+    INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (10, 'Test Name 10', GETDATE());
+    INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (11, 'Test Name 11', GETDATE());
+    INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (12, 'Test Name 12', GETDATE());
+    INSERT INTO MyTable (Id, Name, CreatedAt) VALUES (13, 'Test Name 13', GETDATE());
 
+    CREATE LOGIN dbuser WITH PASSWORD = 'YourStrongPassw0rd';
 
-CREATE LOGIN dbuser WITH PASSWORD = 'YourStrongPassw0rd';
-GO
+    CREATE USER dbuser FOR LOGIN dbuser;
 
-CREATE USER dbuser FOR LOGIN dbuser;
-GO
+    GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::dbo TO dbuser;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::dbo TO dbuser;
-GO
+END
