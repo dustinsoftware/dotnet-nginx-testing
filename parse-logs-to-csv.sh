6dc6251f1f0845bc0d46b5ae64660bec3b9163ca
Dustin Masters <dustin.masters@olo.com>
2025-01-31 12:12:46 -0500

    format log name in table

--- parse-logs-to-csv.sh
+++ parse-logs-to-csv.sh
@@ -10,6 +10,7 @@ echo "directory,100 connections,500 connections,1000 connections,2000 connection
 find . -name "bombardier-container.log" | while read log_file; do
   # Extract the directory name
   dir_name=$(dirname "$log_file")
+  dir_name=${dir_name#./logs/thread-pool-limiter-}
   # Extract the 2xx and 5xx counts using grep and awk
   counts_2xx=($(grep -o '2xx - [0-9]\+' "$log_file" | awk '{print $3}'))
   counts_5xx=($(grep -o '5xx - [0-9]\+' "$log_file" | awk '{print $3}'))
@@ -40,6 +41,8 @@ echo "|-----------|-----------------|-----------------|------------------|------
 tail -n +2 $output_csv_5xx | while IFS=, read -r directory c100 c500 c1000 c2000; do
     echo "| $directory | $c100 | $c500 | $c1000 | $c2000 |" >> $markdown_file
 done
+
+
 
 # Copy the markdown file to the GitHub step summary
 cp $markdown_file $GITHUB_STEP_SUMMARY
